3889c6d5213ed2d2be841e826895fdd07c10455a
Jonathan Prabakar <116781418+jonathan-prabakar@users.noreply.github.com>
2026-08-19 19:09:03 -0500

    Update personal website URL in resume data

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -15,7 +15,7 @@ import { TailwindCSS } from "@/components/ui/svgs/tailwind";
 export const DATA = {
   name: "Jonathan Gladwin Prabakar - CS + Statistics student",
   initials: "JP",
-  url: "https://alexmercer.dev",
+  url: "https://astrofolio.jonprabakar9.workers.dev/",
   location: "San Antonio, TX",
   locationLink: "https://www.google.com/maps/place/san_antonio+tx",
   description:
